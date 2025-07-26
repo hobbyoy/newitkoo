8ec174d1bbbc93d54d9e7032aad76a542eba95db
Hobbyoy <wwhw7712@gmail.com>
2025-07-27 03:23:55 +0900

    0

--- src/components/tab3/DriverFeeBox.tsx
+++ src/components/tab3/DriverFeeBox.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 export default function DriverFeeBox({ deductions, onChange }: Props) {
   return (
-    <div className="w-[350px] h-[275px] rounded-[20px] shadow-lg bg-gradient-to-br from-[#f87171] via-[#ef4444] to-[#b91c1c] text-white p-5 flex flex-col justify-between">
+    <div className="w-[350px] h-[275px] rounded-[20px] shadow-lg bg-gradient-to-br from-[#FF3538] via-[#ef4444] to-[#FF3538] text-white p-5 flex flex-col justify-between">
       <h3 className="text-center text-[24px] font-semibold text-black">기사부담 비용 입력</h3>
 
       <div className="flex flex-col gap-2 text-sm">
