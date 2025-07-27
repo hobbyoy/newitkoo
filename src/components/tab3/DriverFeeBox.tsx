6c6de0dc666641693fd08d7fc2b2d22aae13d6bc
Hobbyoy <wwhw7712@gmail.com>
2025-07-27 12:45:41 +0900

    1

--- src/components/tab3/DriverFeeBox.tsx
+++ src/components/tab3/DriverFeeBox.tsx
@@ -15,10 +15,8 @@ interface Props {
 
 export default function DriverFeeBox({ deductions, onChange }: Props) {
   return (
-    <div className="w-[350px] h-[275px] rounded-[20px] shadow-lg bg-gradient-to-br from-[#FF3538] via-[#ef4444] to-[#FF3538] text-white p-5 flex flex-col justify-between">
-      <h3 className="text-center text-[24px] font-semibold text-black">기사부담 비용 입력</h3>
-
-      <div className="flex flex-col gap-2 text-sm">
+<div className="absolute top-[200px] left-[650px] w-[350px] h-[275px] rounded-[25px] border-2 border-white bg-[linear-gradient(180deg,_rgba(255,255,255,0.2)_0%,_rgba(255,255,255,0)_100%),_#FF3538] backdrop-blur-[2px] text-white p-4 shadow-lg z-10">      <h3 className="text-center text-[24px] font-semibold text-black">기사부담 비용 입력</h3>
+ <div className="flex flex-col gap-2 text-sm">
         {[
           ['고용보험', 'empDeduct'],
           ['산재보험', 'indDeduct'],
@@ -33,7 +31,7 @@ export default function DriverFeeBox({ deductions, onChange }: Props) {
               placeholder="0"
               value={deductions[key as keyof Deductions] ?? ''}
               onChange={(e) => onChange(key as keyof Deductions, e.target.value)}
-              className="w-[120px] px-2 py-1 rounded text-black text-right"
+              className="w-[240 px] px-2 py-1 rounded text-black text-right border border-white bg-white"
             />
           </div>
         ))}
